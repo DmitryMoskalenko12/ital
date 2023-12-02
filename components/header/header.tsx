f90196d86f797a7752a04fff6b4615cd45591229
Dima Moskalenko <dmtrmsklnk@gmail.com>
2023-12-02 19:53:15 +0200

    save

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,18 +1,48 @@
 import Image from "next/image";
-const Header = () => {
+import { useTranslation } from "next-i18next";
+import { useState } from "react";
+import React, { useRouter } from "next/router";
+import Link from "next/link";
+
+const Header: React.FC = () => {
+    const [active, setActive] = useState<boolean>(false);
+    const {locale, locales, push} = useRouter();
+    const {t} = useTranslation('main');
+
+    const onActive = () => {
+      setActive(active => !active);
+    }
+
     return (
-        <header className="pt-[15px] px-[87px] bg-[url(/images/bg-main.png)] bg-cover bg-center bg-no-repeat min-h-[800px]">
+        <header className="pt-[15px] px-[40px] bg-[url(/images/bg-main.png)] bg-cover bg-center bg-no-repeat min-h-[800px]">
            <div className="container">
-            <div className="w-[85px] h-[85px] ">
-             <Image src={'/images/logo.png'} width={200} height={200} alt="logo"/>
+            <div className="flex justify-between items-center">
+            <div className="w-[85px] h-[85px]">
+             <Image className=" w-[100%] h-[100%] object-contain" src={'/images/logo.png'} width={200} height={200} alt="logo"/>
             </div>
             
-            <div className="">
-                <svg xmlns="http://www.w3.org/2000/svg" width="60" height="16" viewBox="0 0 60 16" fill="none">
+            <nav className={`nav-style ${active ? '-translate-x-[0]': '-translate-x-[100%]'}`}>
+                <ul className="h-full gap-[10px] flex items-center justify-center flex-col">
+                    <li><a className=" relative text-white text-[18px] font-normal hover:after:bg-hakki after:absolute after:w-full after:h-[1px] after:mb-[-2px] after:bottom-0 after:left-0" href="#">{t('our-project')}</a></li>
+                    <li><a className=" text-white text-[18px] font-normal" href="#">{t('media')}</a></li>
+                    <li><a className=" text-white text-[18px] font-normal" href="#">{t('about-us')}</a></li>
+                    <li><a className=" text-white text-[18px] font-normal" href="#">{t('contacts')}</a></li>
+                    <li className=" ">
+                        {
+                            locales?.map(l => {
+                                return <Link key={l} className=" text-white text-[18px] font-normal" data-lang={l} href={'/'} locale={l}>{l === 'en' ? t('En') : t('Uk')} </Link>
+                            })
+                        }
+                    </li>
+                </ul>
+            </nav>
+            <div className=" cursor-pointer mb-5" onClick={onActive}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="60" height="16" viewBox="0 0 60 16" fill="none">
                     <rect width="60" height="2" fill="#D8B152"/>
                     <rect y="14" width="60" height="2" fill="#D8B152"/>
                     <rect x="16" y="7" width="44" height="2" fill="#D8B152"/>
-                </svg>
+              </svg>
+            </div>
             </div>
            </div>
         </header>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 export default function Home() {
   return (
     <>
     </>
   )
+}
+
+export async function getStaticProps({locale}: {locale: string}) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['main']))
+    }
+  }
 }
